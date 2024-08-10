461a23b9dfe0bf852422ebc3d2eebfaeb352e8d2
Fernando Barroso <fernandobarroso@suaquadra.com.br>
2024-08-09 22:48:57 -0700

    ...

--- src/app/(logged)/layout.tsx
+++ src/app/(logged)/layout.tsx
@@ -1,13 +1,11 @@
 import HorizontalBalanceCard from "@/app/(logged)/components/BalanceCard";
-import { currentUser } from "@clerk/nextjs/server";
-import { createUser, getUser } from "@/app/utils/requests/users";
 
 export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  
+
   return (
     <div className="flex flex-col w-full min-h-[83vh]">
       <HorizontalBalanceCard />
