c18149b072193d613540e1376cc84288bbb5063b
Fernando Barroso <fernandobarroso@suaquadra.com.br>
2024-08-10 01:10:58 -0700

    ...

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,17 @@
-import { authMiddleware } from "@clerk/nextjs/server";
- 
-export default authMiddleware({
-    publicRoutes: ['/', '/deposit', '/withdraw', '/loans', '/payments', '/transfer', '/crypto', '/alldata'],
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/deposit(.*)", "/withdraw(.*)"]);
+
+export default clerkMiddleware((auth, req) => {
+  if (!isPublicRoute(req)) {
+    auth().protect();
+  }
 });
- 
+
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
 };
