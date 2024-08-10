5897912d487928526dccba82ef8c0dcc98ee86d4
Fernando Barroso <fernandobarroso@suaquadra.com.br>
2024-08-10 00:39:31 -0700

    ...

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,8 +1,6 @@
 import { SignIn } from "@clerk/nextjs";
-import { currentUser } from "@clerk/nextjs/server";
 
 export default async function SignInPage() {
-  const user = await currentUser();
   return (
     <div className="flex justify-center py-24">
       <SignIn />
