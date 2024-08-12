f212f5026190105ac993426f46ced759b0f95b44
Fernando Barroso <fernandobarroso@suaquadra.com.br>
2024-08-11 23:09:00 -0700

    fix usdc fiueld

--- src/app/(logged)/components/BalanceCard/index.tsx
+++ src/app/(logged)/components/BalanceCard/index.tsx
@@ -77,7 +77,7 @@ export default async function HorizontalBalanceCard() {
           <p>
             {(field === "BTC" || field === "ETH")
               ? balances[field as TransactionEntity]?.toFixed(8) || 0 : field === "USDC" ? 
-              `${Math.abs(parseInt(balances[field as TransactionEntity]?.toFixed(0)) )}` || 0 :
+              (balances[field as TransactionEntity]?.toFixed(0)) || 0 :
               `$ ${Math.abs(parseInt(balances[field as TransactionEntity]?.toFixed(0)) )}` || 0}
           </p>
         </div>
